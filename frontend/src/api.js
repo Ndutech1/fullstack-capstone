cebf37994aabb7baf5fa5a6b22f02368d4a70c2e
Ndutech1 <136145477+Ndutech1@users.noreply.github.com>
2025-06-10 21:06:33 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://fullstack-capstone-1w3v.onrender.com',
+  withCredentials: true, // optional, for cookies
 });
 
 API.interceptors.request.use((req) => {
