3dc80dac12e046f2e9066201063faee664047763
DUBEM <ndutechcompcity@gmail.com>
2025-06-12 22:31:16 -1100

    commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,23 @@
-router.get('/me', auth, async (req, res) => {
-  const user = await User.findById(req.user.id).select('-password');
-  const favorites = await Favorite.find({ userId: req.user.id });
-  const watchlist = await Watchlist.find({ userId: req.user.id });
-  const reviews = await Review.find({ userId: req.user.id });
+const express = require('express');
+const router = express.Router();
 
-  res.json({ user, favorites, watchlist, reviews });
+const auth = require('../middleware/auth');
+const User = require('../Models/User');
+const Favorite = require('../Models/Favorite');
+const Watchlist = require('../Models/Watchlist');
+const Review = require('../Models/Review');
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const favorites = await Favorite.find({ userId: req.user.id });
+    const watchlist = await Watchlist.find({ userId: req.user.id });
+    const reviews = await Review.find({ userId: req.user.id });
+
+    res.json({ user, favorites, watchlist, reviews });
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
 });
+
+module.exports = router;
